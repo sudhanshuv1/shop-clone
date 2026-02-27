bb3779ad5250c5f04098fd343aeebba6d16fef67
Sudhanshu Tiwari <sudhanshutiwari@ourguide.ai>
2026-02-27 23:37:15 +0530

    Change data-api-url

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
           </AppProvider>
         </AuthProvider>
         <Script 
-          src="/ourguide-b2b-widget.iife.js"
+          src="https://ourguide-b2-b.vercel.app/ourguide-b2b-widget.iife.js"
           data-api-url="https://ourguide-b2-b.vercel.app"
           data-product-id="prod_ba70c68d-5282-4da1-ba99-c46daddf4fa3"
           data-agent-name="Assistant"
